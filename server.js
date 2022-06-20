fb407c20ec201b593299f400f8bd23117704cd0d
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-20 10:28:10 +0200

    old view is out

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { siteData } from './src/models.js';
-import { siteView } from './src/views.js';
 import path from 'path';
 
 const __dirname = path.resolve(path.dirname(''));
@@ -12,15 +11,10 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './public/views'));
-console.log(siteData);
+
 app.get('/', (req, res) => {
     res.render('pages/index', siteData)
 });
-
-
-// app.get('/', (req, res) => {
-//     res.send(siteView(siteData));
-// });
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
